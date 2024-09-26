961bb9e3952e42e9782e9f34b8a7cd12b4cdfe43
Julia Mayumi <julia.mayumi.costa@escola.pr.gov.br>
2024-09-26 08:16:50 -0300

    jg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function criarCartas(categoria, perguntas, respostas) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHtml = ` 
+    cartao.innerHTML = ` 
     <div class="cartao-conteudo">
     <h3>${categoria}</h3>
     <div class = "cartao-pergunta">
