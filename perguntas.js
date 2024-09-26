630212c77f46d48c258524d7b7bc16f8951b8411
Julia Mayumi <julia.mayumi.costa@escola.pr.gov.br>
2024-09-26 07:29:11 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,9 +1,118 @@
-criaCartao(
-    'historia',
-    'quando ocorreu a revolucao francesa?',
-    'r=1789')
 
-criaCartao(
-    'historia',
-    'quando foi o governo vargas?',
-    'r=1951')
+
+criarCartas (
+'Japonês' ,
+' 一 （いち）' ,
+' 1 ')
+
+criarCartas (
+'Japonês' ,
+' 二 （に）' ,
+' 2 ')
+
+criarCartas (
+'Japonês' ,
+' 三 （さん）' ,
+' 3 ')
+
+criarCartas (
+'Japonês' ,
+' 四 （ よん / し） ' ,
+' 4')
+
+criarCartas (
+'Japonês' ,
+' 五 （ご）' ,
+' 5 ')
+
+criarCartas (
+'Japonês' ,
+' 六（ろく） ' ,
+' 6 ')
+
+criarCartas (
+'Japonês' ,
+' 七 （なな / しち）' ,
+' 7 ')
+
+criarCartas (
+'Japonês' ,
+' 八 （はち）' ,
+' 8 ')
+
+criarCartas (
+'Japonês' ,
+' 九 （きゅう）' ,
+' 9 ')
+
+criarCartas (
+'Japonês' ,
+' 十 （じゅう） ' ,
+' 10 ')
+
+criaCartas (
+
+'Biologia',
+'Coanócitos',
+'Células que fazem a digestão intracelular dos poríferos, além da circulação de água, )
+
+criaCartas (
+
+'Biologia',
+'Cordados',
+'Possuem notocorda / coluna vertebral', )
+
+criaCartas (
+
+'Biologia',
+'Ectotérmicos, o que é?',
+'Precisam de gatilho externo para a manutenção da sua temperatura', )
+
+criaCartas (
+
+'Biologia',
+'Heterotróficos',
+'Não mantém temperatura constante durante o dia', )
+
+criaCartas (
+
+'Biologia',
+'Chondrichtyes (peixes)',
+'Esqueleto cartilaginoso', )
+
+criaCartas (
+
+'Biologia',
+'Ampolas de Lorenzini',
+'Eletroreceptores (peixes, Chondricthyes)', )
+
+criaCartas (
+
+'Biologia',
+'Reprodução platelmintos',
+'Di e monoica, interna e externa, direto e indireto, sexuada e assexuada', )
+
+criaCartas (
+
+'Biologia',
+'Arthropoda, especificidade do geral',
+'Exoesqueleto quitinoso, conquista ambiente terrestre.', )
+
+criaCartas (
+
+'Biologia',
+'Ecdiase',
+'Troca do exoesqueleto dos arthropodas', )
+
+criaCartas (
+
+'Biologia',
+'Sist. Respiratório hexapoda',
+'Traqueal', )
+
+criaCartas (
+
+'Biologia,
+'Sist. Excretor Crustacea',
+'Glândulas verdes ou antenais', )
+
